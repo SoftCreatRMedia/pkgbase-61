672c89afd96ea70ae00b5b4fc7e9d0204c024ac1
Sascha Greuel <github@1-2.dev>
2024-09-08 18:58:45 +0200

    Update

--- pkgbase-61-self-hosted.sh
+++ pkgbase-61-self-hosted.sh
@@ -4,9 +4,10 @@ rm -rf pkgbase
 git clone git@github.com:SoftCreatRMedia/pkgbase-61-self-hosted.git pkgbase
 rm -rf pkgbase/.git
 rm -rf .github
+rm -f .gitignore
 rm -f .eslintignore
 rm -f .eslintrc.js
-r, -f eslint.config.mjs
+rm -f eslint.config.mjs
 rm -f .php-cs-fixer.dist.php
 rm -f .phpcs.xml
 rm -f .prettierrc
