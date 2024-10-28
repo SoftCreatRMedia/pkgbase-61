32cd368e3602021be988cd76d9f58baeef922d18
Sascha Greuel <github@1-2.dev>
2024-10-28 11:07:14 +0100

    Updated deps

--- pkgbase-61-self-hosted.sh
+++ pkgbase-61-self-hosted.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 rm -rf pkgbase
-git clone git@github.com:SoftCreatRMedia/pkgbase-61-self-hosted.git pkgbase
+git clone https://github.com/SoftCreatRMedia/pkgbase-61-self-hosted.git pkgbase
 rm -rf pkgbase/.git
 rm -rf .github
 rm -f .gitignore
